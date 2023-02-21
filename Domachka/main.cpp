3d05d6ab5059e841457b134aac4c774991351504
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-02-22 00:32:52 +0500

    Remont

--- Domachka/main.cpp
+++ Domachka/main.cpp
@@ -5,25 +5,53 @@ using  std :: cout;
 
 
 #define tab "\t"; 
-// Функция и перегрузка функции 
+const int ROWS = 3; // Rows количество строк 
+const int COLS = 5; // Cows количество столбиков 
+
+// Функция и перегрузка функции + двумерный массив
+//----------------------------------------------------------------------------//
 // Прототип ф-и
 void FillRand(int arr[], const int n); // прототип функции
 void FillRand(double arr[], const int n); 
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS);
+void FillRand(char arr[], const int n); // прототип функции для char
+void FillRand(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS);
+void Print(double arr[ROWS][COLS], const int ROWS, const int COLS);
+void Print(char arr[], const int n);
+void Print(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 int Sum(int arr[], const int n);
 double Sum(double arr[], const int n);
+int Sum(int arr[ROWS][COLS], const int ROWS, const int COLS);
+double Sum(double arr[ROWS][COLS], const int ROWS, const int COLS);
+int Sum(char arr[], const int n);
+int Sum(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 double AVG(int arr[], const int n);
 double AVG(double arr[], const int n);
+double AVG(int arr[ROWS][COLS], const int ROWS, const int COLS);
+double AVG(double arr[ROWS][COLS], const int ROWS, const int COLS);
+double AVG(char arr[], const int n);
+double AVG(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 int MinValueIn(int arr[], const int n);
 double MinValueIn(double arr[], const int n);
+int MinValueIn(int arr[ROWS][COLS], const int ROWS, const int COLS);
+double MinValueIn(double arr[ROWS][COLS], const int ROWS, const int COLS);
+char MinValueIn(char arr[], const int n);
+char MinValueIn(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 int MaxValueIn(int arr[], const int n);
 double MaxValueIn(double arr[], const int n);
+int MaxValueIn(int arr[ROWS][COLS], const int ROWS, const int COLS);
+double MaxValueIn(double arr[ROWS][COLS], const int ROWS, const int COLS);
+char MaxValueIn(char arr[], const int n);
+char MaxValueIn(char arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void ShiftLeft(int arr[], const int n, int number_of_shifts=0);
 void ShiftLeft(double arr[], const int n, int number_of_shifts=0);
@@ -33,6 +61,9 @@ void ShiftRight(double arr[], const int n, int sdvg=0);
 
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
+void Sort(char arr[], const int n);
+
+//void Sort(int arr[ROWS][COLS], const int ROWS, const int COLS);
 
 void main()
 {
@@ -40,8 +71,6 @@ void main()
 	
 	const int n = 10;
 	int arr[n];
-	
-	
 	// Сумма элементов массива 
 	FillRand(arr, n);
 	Print(arr, n);
@@ -60,17 +89,14 @@ void main()
 	cout << "Введите количество сдвига: "; cin >> number_of_shifts;
 	ShiftLeft(arr, n, number_of_shifts);
 	Print(arr, n);
-	
 	// сдвиг вправо 
 	int sdvg=0;
 	cout << "Введите количество сдвига: "; cin >> sdvg;
 	ShiftRight(arr, n, sdvg);
 	Print(arr, n);
-	
+	//========================================================//
 	const int Size = 8;
 	double brr[Size];
-	
-	
 	FillRand(brr, Size);
 	Print(brr, Size);
 	Sort(brr, Size);
@@ -81,9 +107,63 @@ void main()
 	cout << "Средне-арифметическое элементов массива: " << AVG(brr, Size) << endl;
 	MinValueIn(brr, Size);
 	cout << "Минимальное значение массива: " << MinValueIn(brr, Size) << endl;
-	MaxValueIn(arr, n);
+	MaxValueIn(brr, Size);
 	cout << "Максимальное значение массива: " << MaxValueIn(brr, Size) << endl;
+	//-------------------------------------------------------//
+	// Двумерный массив
+	int i_arr_2[ROWS][COLS];// i массив интовых элементов 
+	FillRand(i_arr_2, ROWS, COLS);
+	Print(i_arr_2, ROWS, COLS);
+	Sum(i_arr_2, ROWS, COLS);
+	cout << "Сумма элементов массива: " << Sum(i_arr_2, ROWS, COLS) << endl;
+	AVG(i_arr_2, ROWS, COLS);
+	cout << "Средне-арифметическое элементов массива: " << AVG(i_arr_2, ROWS, COLS) << endl;
+	MinValueIn(i_arr_2, ROWS,COLS);
+	cout << "Минимальное значение массива: " << MinValueIn(i_arr_2, ROWS, COLS) << endl;
+	MaxValueIn(i_arr_2, ROWS, COLS);
+	cout << "Максимальное значение массива: " << MaxValueIn(i_arr_2, ROWS, COLS) << endl;
+
+	double d_brr_2[ROWS][COLS]; // массив для дубля
+	FillRand(d_brr_2, ROWS, COLS);
+	Print(d_brr_2, ROWS, COLS);
+	Sum(d_brr_2, ROWS, COLS);
+	cout << "Сумма элементов массива: " << Sum(d_brr_2, ROWS, COLS) << endl;
+	AVG(d_brr_2, ROWS, COLS);
+	cout << "Средне-арифметическое элементов массива: " << AVG(d_brr_2, ROWS, COLS) << endl;
+	MinValueIn(d_brr_2, ROWS, COLS);
+	cout << "Минимальное значение массива: " << MinValueIn(d_brr_2, ROWS, COLS) << endl;
+	MaxValueIn(d_brr_2, ROWS, COLS);
+	cout << "Максимальное значение массива: " << MaxValueIn(d_brr_2, ROWS, COLS) << endl;
+
+	//для чаррр=====================================
+	const int Value= 10; 
+	char crr[Value];
+	FillRand(crr, Value);
+	Print(crr, Value);
+	Sort(crr, Value);
+	Print(crr, Value);
+	Sum(crr, Value);
+	cout << "Сумма элементов массива: " << Sum(crr, Value) << endl;
+	AVG(crr, Value);
+	cout << "Средне-арифметическое элементов массива: " << AVG(crr, Value) << endl;
+	MinValueIn(crr, Value);
+	cout << "Минимальное значение массива: " << MinValueIn(crr, Value) << endl;
+	MaxValueIn(crr, Value);
+	cout << "Максимальное значение массива: " << MaxValueIn(crr, Value) << endl;
+
+	char TAB[ROWS][COLS]; // двухмерник для чара
+	FillRand(TAB, ROWS, COLS);
+	Print(TAB, ROWS, COLS);
+	Sum(TAB, ROWS, COLS);
+	cout << "Сумма элементов массива: " << Sum(TAB, ROWS, COLS) << endl;
+	AVG(TAB, ROWS, COLS);
+	cout << "Средне-арифметическое элементов массива: " << AVG(TAB, ROWS, COLS) << endl;
+	MinValueIn(TAB, ROWS, COLS);
+	cout << "Минимальное значение массива: " << MinValueIn(TAB, ROWS, COLS) << endl;
+	MaxValueIn(TAB, ROWS, COLS);
+	cout << "Максимальное значение массива: " << MaxValueIn(TAB, ROWS, COLS) << endl;
 }
+
 
 // Случайные числа массива 
 void FillRand(int arr[], const int n)
@@ -103,7 +183,47 @@ void FillRand(double arr[], const int n)
 	}
 	cout << endl;
 }
-
+// заполнение двумерного массива случайными числами
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+void FillRand(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 10000;
+			arr[i][j] /= 100;
+		}
+	}
+	cout << endl;
+}
+void FillRand(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+	cout << endl;
+}
+void FillRand(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
 // Вывод массива 
 void Print(int arr[], const int n)
 {
@@ -121,6 +241,50 @@ void Print(double arr[], const int n)
 	}
 	cout << endl;
 }
+
+// вывод двумерного массива на экран 
+void Print(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+void Print(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+void Print(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Print(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			cout << arr[i][j] << tab;
+		}
+		cout << endl;
+	}
+}
+// Сумма
 int Sum(int arr[], const int n)
 {
 	int sum = 0;
@@ -143,6 +307,53 @@ double Sum(double arr[], const int n)
 	return sum;
 	cout << endl;
 }
+int Sum(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			sum += arr[i][j];
+		}
+	}return sum; 
+	cout << endl; 
+}	
+double Sum(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	double sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			sum += arr[i][j];
+		}
+	}return sum;
+	cout << endl;
+}
+int Sum(char arr[], const int n)
+{
+	char sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+	}
+	return sum;
+	cout << endl;
+}
+int Sum(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	char sum = 0;
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			sum += arr[i][j];
+		}
+	}return sum;
+	cout << endl;
+}
+// Cреднее значение 
 double AVG(int arr[], const int n)
 {
 	return (double (Sum(arr, n)) / n);
@@ -151,12 +362,27 @@ double AVG(double arr[], const int n)
 {
 	return (double(Sum(arr, n)) / n);
 }
-
-
+double AVG(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	return double(Sum(arr, ROWS,COLS)) / (ROWS*COLS);
+}
+double AVG(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	return double(Sum(arr, ROWS, COLS)) / (ROWS * COLS);
+}
+double AVG(char arr[], const int n)
+{
+	return (double(Sum(arr, n)) / n);
+}
+double AVG(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	return double(Sum(arr, ROWS, COLS)) / (ROWS * COLS);
+}
+// минимальное значение 
 int MinValueIn(int arr[], const int n)
 {
 	int min = arr[0];
-	for (int i = 1; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] < min) min = arr[i];
 	}
@@ -166,19 +392,70 @@ int MinValueIn(int arr[], const int n)
 double MinValueIn(double arr[], const int n)
 {
 	double min = arr[0];
-	for (int i = 1; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] < min) min = arr[i];
 	}
 	return min;
 	cout << endl;
 }
+int MinValueIn(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] < min) min = arr[i][j];
+		}
+		
+	}
+	return min;
+	cout << endl;
+}
+double MinValueIn(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	double min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] < min) min = arr[i][j];
+		}
 
+	}
+	return min;
+	cout << endl;
+}
+char MinValueIn(char arr[], const int n)
+{
+	char min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+	}
+	return min;
+	cout << endl;
+}
+char MinValueIn(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	char min = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] < min) min = arr[i][j];
+		}
 
+	}
+	return min;
+	cout << endl;
+}
+// максивальное значение
 int MaxValueIn(int arr[], const int n)
 {
 	int max = arr[0];
-	for (int i = 1; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] > max) max = arr[i];
 	}
@@ -188,14 +465,63 @@ int MaxValueIn(int arr[], const int n)
 double MaxValueIn(double arr[], const int n)
 {
 	double max = arr[0];
-	for (int i = 1; i < n; i++)
+	for (int i = 0; i < n; i++)
 	{
 		if (arr[i] > max) max = arr[i];
 	}
 	return max;
 	cout << endl;
 }
-
+int MaxValueIn(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	int max = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max) max = arr[i][j];
+		}
+	}
+	return max;
+	cout << endl;
+}
+double MaxValueIn(double arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	double max = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max) max = arr[i][j];
+		}
+	}
+	return max;
+	cout << endl;
+}
+char MaxValueIn(char arr[], const int n)
+{
+	char max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max) max = arr[i];
+	}
+	return max;
+	cout << endl;
+}
+char MaxValueIn(char arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	char max = arr[0][0];
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			if (arr[i][j] > max) max = arr[i][j];
+		}
+	}
+	return max;
+	cout << endl;
+}
+// сдвиг влево
 void ShiftLeft(int arr[], const int n, int number_of_shifts)
 {
 	for (int i = 0; i < number_of_shifts; i++)
@@ -222,7 +548,7 @@ void ShiftLeft(double arr[], const int n, int number_of_shifts)
 	}
 	cout << endl;
 }
-
+// сдвиг вправо
 void ShiftRight(int arr[], const int n, int sdvg)
 {
 	for (int i = 0; i < sdvg; i++)
@@ -249,10 +575,9 @@ void ShiftRight(double arr[], const int n, int sdvg)
 	}
 	cout << endl;
 }
-
+// сортировка массива:
 void Sort(int arr[], const int n)
 {
-	// сортировка массива:
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = i + 1; j < n; j++)
@@ -276,6 +601,21 @@ void Sort(double arr[], const int n)
 			if (arr[j] < arr[i])
 			{
 				double buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sort(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
 				arr[i] = arr[j];
 				arr[j] = buffer;
 			}
